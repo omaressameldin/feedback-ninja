d988dd415732c565ee711ebe11a6f5856933c78e
Omar Essam (Inpu) <o.essameldin@gmail.com>
2019-10-18 17:49:31 +0200

    Change feedback route

--- app/main.go
+++ app/main.go
@@ -4,16 +4,16 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/omaressameldin/feedback-ninja/app/pkg/confirmation"
 	"github.com/omaressameldin/feedback-ninja/app/pkg/env"
 	"github.com/omaressameldin/feedback-ninja/app/pkg/feedback"
-	"github.com/omaressameldin/feedback-ninja/app/pkg/confirmation"
 )
 
 func main() {
 	env.ValidateEnvKeys()
 
 	port := env.GetPort()
-	http.HandleFunc("/feedback", feedback.Handler)
+	http.HandleFunc("/", feedback.Handler)
 	http.HandleFunc("/confirmation", confirmation.Handler)
 
 	http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
